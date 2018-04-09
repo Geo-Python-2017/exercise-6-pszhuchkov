8a08ae71722d86af66ca1cc959d6a3d95f371f45
pszhuchkov <pzhuchkov@gmail.com>
2018-04-10 00:07:30 +0400

    Added calculation of monthly average temperatures

--- temperature_anomalies.py
+++ temperature_anomalies.py
@@ -5,9 +5,14 @@ The data contains daily observations obtained from the NOAA Global Historical Cl
 The scripts outputs the number of non-NaN values for average and minimum temperature, 
 the total number of observation days, the first and last observations, average temperature
 for whole file and maximum temperatures for summer of 1969. 
+The script calculates monthly average temperatures.
 
 @author: Pavel
 """
+# The function converts temperature from Fahrenheit to Celsius
+def fahrenheitToCelsius(fahr_temp):
+    converted_temp = (fahr_temp - 32) / 1.8
+    return converted_temp
 # Import pandas
 import pandas as pd
 
@@ -42,6 +47,26 @@ print('The average temperature for all years is', avg_t)
 selection_summer = data.ix[(data['DATE']>=19690501) & (data['DATE']<19690901)]
 selection_summer_tmax = selection_summer['TMAX']
 print(selection_summer_tmax)
+
+# Create year-day column
+data['YD'] = (data['DATE'].astype(str)).str.slice(start=0,stop=6)
+
+# Create empty Dataframe
+monthlyData = pd.DataFrame()
+
+# Group Dataframe
+grouped_month = data.groupby('YD')
+
+# Aggregate data
+for key, group in grouped_month:
+    mean_value = group[['TAVG']].mean()
+    mean_value['YD'] = key
+    mean_value['TempsC'] = fahrenheitToCelsius(mean_value['TAVG']) 
+    monthlyData = monthlyData.append(mean_value, ignore_index=True)
+
+# Print obtained Datframe
+print(monthlyData)
+    
 
 
 
